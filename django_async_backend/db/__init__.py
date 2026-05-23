c12cafd994447ef585beb6f8c619334f2f9189cf
Arfey <Arfey17.mg@gmail.com>
2026-05-24 01:49:23 +0300

    removed initialized_only

--- django_async_backend/db/__init__.py
+++ django_async_backend/db/__init__.py
@@ -4,5 +4,7 @@ async_connections = AsyncConnectionHandler()
 
 
 async def close_old_async_connections(**kwargs):
-    for conn in async_connections.all(initialized_only=True):
+    # don't use initialized_only=True
+    # https://github.com/Arfey/django-async-backend/issues/24
+    for conn in async_connections.all():
         await conn.close()
