82909adbffb9e83e87c154430b1c77b2a0b68b02
Arfey <Arfey17.mg@gmail.com>
2026-07-26 02:52:55 +0300

    added DeleteQuery

--- django_async_backend/db/models/sql/__init__.py
+++ django_async_backend/db/models/sql/__init__.py
@@ -1,10 +1,12 @@
 from django_async_backend.db.models.sql.query import Query
 from django_async_backend.db.models.sql.subqueries import (
+    DeleteQuery,
     InsertQuery,
     UpdateQuery,
 )
 
 __all__ = [
+    "DeleteQuery",
     "InsertQuery",
     "Query",
     "UpdateQuery",
