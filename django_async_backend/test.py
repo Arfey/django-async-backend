59a83a914b6b7377302560ffa3fd4ea48fb51939
Arfey <Arfey17.mg@gmail.com>
2026-08-25 01:01:09 +0300

    added test

--- django_async_backend/test.py
+++ django_async_backend/test.py
@@ -4,6 +4,8 @@ from unittest import IsolatedAsyncioTestCase
 
 from django.core.signals import request_started
 from django.db import reset_queries
+from django.test.utils import modify_settings as _modify_settings
+from django.test.utils import override_settings as _override_settings
 
 from django_async_backend.db import async_connections
 from django_async_backend.db.transaction import async_atomic
@@ -25,6 +27,19 @@ def _refresh_connection_task_ownership_decorator(fn):
 class AsyncioTransactionTestCase(IsolatedAsyncioTestCase):
     databases = "__all__"
 
+    _overridden_settings = None
+    _modified_settings = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        if cls._overridden_settings:
+            cls.enterClassContext(
+                _override_settings(**cls._overridden_settings)
+            )
+        if cls._modified_settings:
+            cls.enterClassContext(_modify_settings(cls._modified_settings))
+
     def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
 
@@ -43,6 +58,12 @@ class AsyncioTransactionTestCase(IsolatedAsyncioTestCase):
                 name,
                 _refresh_connection_task_ownership_decorator(method),
             )
+
+    def settings(self, **kwargs):
+        return override_settings(**kwargs)
+
+    def modify_settings(self, **kwargs):
+        return modify_settings(**kwargs)
 
     def _callSetUp(self):
         self._asyncioRunner.get_loop()
@@ -108,6 +129,29 @@ class AsyncioTestCase(AsyncioTransactionTestCase):
             )
         )
         self._asyncioTestContext.run(self.tearDown)
+
+
+def _save_options_for_supported_class(decorator, cls):
+    from django.test import SimpleTestCase
+
+    if not issubclass(cls, (SimpleTestCase, AsyncioTransactionTestCase)):
+        raise ValueError(
+            "Only subclasses of Django SimpleTestCase or "
+            "AsyncioTransactionTestCase can be decorated with %s"
+            % type(decorator).__name__
+        )
+    decorator.save_options(cls)
+    return cls
+
+
+class override_settings(_override_settings):
+    def decorate_class(self, cls):
+        return _save_options_for_supported_class(self, cls)
+
+
+class modify_settings(_modify_settings):
+    def decorate_class(self, cls):
+        return _save_options_for_supported_class(self, cls)
 
 
 class AsyncCaptureQueriesContext:
